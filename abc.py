5a2f5b28d7acfd4810f00a684230514349788f9e
ZXZ <541459574@qq.com>
2019-05-21 13:59:01 +0800

    20190521

--- abc.py
+++ abc.py
@@ -2,13 +2,19 @@ class Stack():
     def __init__(self):
         self.stack = []
 
+        def size(self):
+            return len(self.stack)
+
+    def is_empty(self):
+        return self.size() == 0
+
     def size(self):
         return len(self.stack)
 
     def is_empty(self):
         return self.size() == 0
 
-    def push(self,data):
+    def push(self, data):
         self.stack.append(data)
 
     def pop(self):
@@ -29,8 +35,17 @@ class Stack():
     def clean(self):
         del self.stack[:]
 
+        def clean2(self):
+            self.stack = []
 
 
+s = Stack()
+s.push(1)
+s.puah(2)
+s.prtStack()
+print(s.size())
+s.clean2()
+print(s.size())
 
 s = Stack()
 
@@ -41,3 +56,7 @@ s.prtStack()
 s.clean()
 s.prtStack()
 print(s.size())
+
+
+
+##  PB  JavaScript   C# 堆栈，  判断括号，  逆波兰表达式/计算，  big O(1)栈内最小值
